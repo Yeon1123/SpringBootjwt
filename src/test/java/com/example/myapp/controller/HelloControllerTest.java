977da11bb3685a0f6b1a2a677272b4aeab611a20
유정연 <jyyu@magis.co.kr>
2022-03-07 22:16:35 +0900

    asciidoc 사용 설정 추가

--- src/test/java/com/example/myapp/controller/HelloControllerTest.java
+++ src/test/java/com/example/myapp/controller/HelloControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
@@ -11,19 +12,23 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.fasterxml.jackson.core.PrettyPrinter;
+
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
 
 @WebMvcTest
 @ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs(outputDir = "target/generated-snippets")
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
-    //restDocumentation을 사용하기 위한 설정
+    // restDocumentation을 사용하기 위한 설정
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
         this.mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
@@ -31,8 +36,8 @@ public class HelloControllerTest {
                 .build();
     }
 
-    //"index" 라는 디렉토리에 snippet을 추가함.
-    //output 디렉토리를 설정해주지 않았으므로 디폴트 target/generated-snippets 하위에 생성된다
+    // "index" 라는 디렉토리에 snippet을 추가함.
+    // output 디렉토리를 설정해주지 않았으므로 디폴트 target/generated-snippets 하위에 생성된다
     @Test
     public void returnHelloWorld() throws Exception {
         String hello = "hello world!";
@@ -40,6 +45,11 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello))
-                .andDo(document("index"));
+                .andDo(document("index", responseFields(
+                        fieldWithPath("concat.email").description("this is user email!")
+                        )
+                    )
+                ); 
+                // target/generated-snippets/index아래에 생성하고, 응답 payload를 받기 위해 사용
     }
 }
