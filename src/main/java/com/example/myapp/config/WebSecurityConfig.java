a9dc3ff317b236e734778e4f97b9b6e76c5e0ef1
유정연 <jyyu@magis.co.kr>
2022-03-14 22:29:10 +0900

    postgresql 연동, spring security 적용

---
+++ src/main/java/com/example/myapp/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.myapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    //보안 설정: url별로 접근 권한을 줄 수 있다. 루트 페이지(기본홈) 을 제외한 모든 페이지는
+    //루트 페이지 이외의 모든 사이트는 .anyRequest()authenticated(): 로그인이 되어야 한다.
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                // ("/") 페이지 이외의 요청은 무조건 login으로 이동되고 permit all 설정을 준다.
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
+    }
+}
